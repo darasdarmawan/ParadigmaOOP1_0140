11583f4c39eeec6d64851ab17a52f6ed7b30eb2b
darasdarmawan <darasyauqidarmawan@gmail.com>
2024-05-20 11:12:56 +0700

    membuat class mahasiswa

--- PemdasRelasi/PemdasRelasi.cpp
+++ PemdasRelasi/PemdasRelasi.cpp
@@ -2,4 +2,11 @@
 #include <string>
 using namespace std;
 
+#include "jantung.h"
+#include "manusia.h"
 
+int main() {
+	manusia* varManusia = new manusia("jono";
+	delete varManusia;
+	return 0;
+}
