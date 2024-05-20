6e1ea61c95dd9469e821f95ee73869e6a463488e
darasdarmawan <darasyauqidarmawan@gmail.com>
2024-05-20 11:20:56 +0700

    membuat akses modifier public pd class anak

--- Aggregation/anak.h
+++ Aggregation/anak.h
@@ -1,7 +1,13 @@
 #pragma once
 #ifndef ANAK_H
 #define ANAK_H
+#include <iostream>
+using namespace std;
 
 class anak {
-
+public: 
+	string nama;
+	anak(string pNama) :nama(pNama) {
+		cout << "Anak \" " << nama << "\" ada\n";
+	}
 };
